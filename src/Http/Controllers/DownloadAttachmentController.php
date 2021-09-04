a804011c3dec4f79d62de961607385d6aa4b20bc
Sina Rajabpour <sina.rajabpour@outlook.com>
2021-09-04 11:25:13 +0430

    Init commit

---
+++ src/Http/Controllers/DownloadAttachmentController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Sinarajabpour1998\Attachment\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class DownloadAttachmentController extends Controller
+{
+    public function generate(Request $request)
+    {
+        return Storage::disk('private')->download($request->path);
+    }
+}
